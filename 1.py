ccf39b990fbd9c1763b140e430af8e7fddc46163
Juan Camilo Espitia <30874068+20152020040@users.noreply.github.com>
2018-02-19 16:42:44 -0500

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-def Cociente(n,m):
-    if n<m:
-        return 0
-    else:
-        return 1+Cociente(n-m,m)
-    
-n=int(input("ingrese el numero 1: "))
-m=int(input("ingrese el numero 2: "))
-print("el resultado es ",Cociente(n,m))
