f428a3d7d275055192945688a038a03e8115ec8a
Juan Camilo Espitia <30874068+20152020040@users.noreply.github.com>
2018-02-19 16:43:11 -0500

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-def sumaDig(n):
-    if n<10:
-        return n
-    else:
-        return int(sumaDig(n/10)+sumaDig(n%10))
-    
-n=int(input("ingrese el numero 1: "))
-
-print("el resultado es ",sumaDig(n))
