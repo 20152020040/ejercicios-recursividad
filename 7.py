4d4afc34b51e5cc19f54d8b0a6f015ccf3ab0a38
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2018-02-19 16:43:03 -0500

    Delete 7.py

--- 7.py
+++
@@ -1,9 +0,0 @@
-def longitud(n):
-    if n<=10:
-        return 1
-    else:
-        return int(1+longitud(n/10))
-    
-n=int(input("ingrese el numero 1: "))
-
-print("el resultado es ",longitud(n))
